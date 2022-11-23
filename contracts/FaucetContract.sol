90674e3d44d484ba81e11b553815e70df52c4f40
Juan Rodriguez Moreno <juanseromo12@gmail.com>
2022-11-23 14:43:08 -0500

    last few changes

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,10 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint public funds = 1000; // positive values only
-    int public counter = -10;
-    uint32 public test = 4294967295; //max possible value
-}
-//    uint32 public test = 4294967295
 
+    address[] public funders;
+
+    receive() external payable {}
+
+    function addFunds () external payable {
+        funders.push(msg.sender);
+    }
+
+    function getAllFunders() public view returns (address[] memory){
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+    }
+
+}
